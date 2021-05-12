737d629b28169beb30255f9b0d1aab4b558cf24c
Akshay Bhosale <akshay9594@gmail.com>
2021-05-12 11:14:44 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# The Cetus Project
+Cetus Source to Source compiler improvements being done at University of Delaware
+
+## Bug Fixes and added features
+
+### 1. Handling of loop index initializations within FOR loop declaration-
+        When for loops are declared as- for(int i = 0; i < n; i++), 
+        cetus would remove the initialization of 'i' from it's place and 
+        hoist it at the top of the code block.
+        The reason for doing so would be the use of underlying cetus principle that- 
+        "All variable declarations should appear at the top of the code block". 
+        Cetus wouldn't parallelize a loop without knowing the initial value of the loop index.
+        Fixed this so that instead of having a loop like-  for( ;i < n; i++),
+        We can now have  - for(i = 0 ; i < n; i++) and 'int i' would appear at the top of the block.
+    
+### 2. Added support for logical and bitwise scalar reductions
+        Scalar reductions of the form x = x op expr, where op is any of:
+        {logical AND - && , logical OR- || , Bitwise OR - | , Bitwise AND- & , Bitwise XOR - ^}. 
+        Also added support for Bitwise assignment operators of the form: 
+        {Bitwise AND - &= , Bitwise OR - |= , Bitwise XOR - ^=}
+        
+### 3. Support for Min and Max Reductions
+       OpenMP added the reduction-identifiers "min" and "max" to the reduction clause from OpenMP 3.1.
+       Cetus can now recognize Min and Max reductions implemented using an if-statement or using the
+       Conditional operator (?). Some expression restrictions apply.
+       
+### 4. Support for Multiple reductions using different operators
+       Cetus had no issue recognizing multiple unique reduction statements within the same loop. 
+       But Cetus could not create a separate reduction clause for each reduction-identifier within 
+       the same directive as per the atest OpenMP specification. The support for the same has now been added. 
+       The directive would look something like:
+       
+       Eg. #pragma omp parallel for private(i) reduction(max: maxl)reduction(&: b)reduction(*: e)reduction(+: d)
+           Earlier Cetus would try to include all the identifiers and operators within one reduction clause.
+       
+### 5. Loop Interchange Pass Added to Cetus
+       a. Loop Interchange legality algorithm had some minor bugs which have been fixed.
+       b. Reusability analysis added to the Loop Interchange pass which determines the 
+          best order of loops in the nest for maximizing reusability of cache lines. 
+       c. Model taken from K.S McKinley’s paper- "Optimizing for Parallelism and Data locality".
+       d. Pass can also handle symbolic loop bounds.
+       My contribution was everything else besides the leglity test.
+
+
+    
+    
+  
+            
---
+++ resources/examples/cetus_output/SimpleLoop.c
@@ -0,0 +1,108 @@
+/*
+Copyright (C) 1991-2018 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it andor
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http:www.gnu.org/licenses/>. 
+*/
+/*
+This header is separate from features.h so that the compiler can
+   include it implicitly at the start of every compilation.  It must
+   not itself include <features.h> or any other header that includes
+   <features.h> because the implicit include comes before any feature
+   test macros that may be defined in a source file before it first
+   explicitly includes a system header.  GCC knows the name of this
+   header in order to preinclude it. 
+*/
+/*
+glibc's intent is to support the IEC 559 math functionality, real
+   and complex.  If the GCC (4.9 and later) predefined macros
+   specifying compiler intent are available, use them to determine
+   whether the overall intent is to support these features; otherwise,
+   presume an older compiler has intent to support these features and
+   define these macros by default. 
+*/
+/*
+wchar_t uses Unicode 10.0.0.  Version 10.0 of the Unicode Standard is
+   synchronized with ISOIEC 10646:2017, fifth edition, plus
+   the following additions from Amendment 1 to the fifth edition:
+   - 56 emoji characters
+   - 285 hentaigana
+   - 3 additional Zanabazar Square characters
+*/
+/* We do not support C11 <threads.h>.  */
+/*
+ Very Simple Parallelizable Loop Example
+
+*/
+int main()
+{
+	int a[30000], b[30000], c[30000], d[30000];
+	int i, n, j, count, p, x;
+	int j;
+	int _ret_val_0;
+	n=3000;
+	p=1;
+	count=3;
+	#pragma cetus private(i, j) 
+	#pragma loop name main#0 
+	#pragma cetus reduction(+: p) 
+	for (j=0; j<n; j ++ )
+	{
+		#pragma cetus private(i) 
+		#pragma loop name main#0#0 
+		#pragma cetus reduction(+: p) 
+		#pragma cetus parallel 
+		#pragma omp parallel for if((10000<(1L+(7L*n)))) private(i) reduction(+: p)
+		for (i=0; i<n; i ++ )
+		{
+			if (c[i]!=0)
+			{
+				b[i]=i;
+				if (x!=0)
+				{
+					a[i]=c[i];
+				}
+				p ++ ;
+			}
+		}
+		d[j]=p;
+	}
+	/* for( i = 0 ; i < n; i++){ */
+		/*    a[i] = i + 1; */
+	/* } */
+	#pragma cetus private(j) 
+	#pragma loop name main#1 
+	#pragma cetus parallel 
+	#pragma omp parallel for if((10000<(1L+(5L*n)))) private(j)
+	for (j=0; j<n; j ++ )
+	{
+		if ((j%2)==0)
+		{
+			a[j]=0;
+		}
+		else
+		{
+			a[j]=1;
+		}
+	}
+	#pragma cetus private(j) 
+	#pragma loop name main#2 
+	for (j=0; j<n; j ++ )
+	{
+		a[j]=(a[j-1]+a[j]);
+	}
+	_ret_val_0=0;
+	return _ret_val_0;
+}
